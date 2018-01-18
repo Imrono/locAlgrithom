c31ced5c9ee4936940ad61ded4ecdad8cc542f52
Kang CHEN <rono_back@yeah.net>
2018-01-18 17:06:08 +0800

    locAlgrithom

---
+++ __clean.bat
@@ -0,0 +1,15 @@
+@echo off
+echo delete .\x64\
+rd /s /q .\x64
+echo delete .\debug\
+rd /s /q .\debug
+echo delete .\release\
+rd /s /q .\release
+echo delete .\GeneratedFiles\
+rd /s /q .\GeneratedFiles
+echo delete .\build-locAlgrithom-Desktop_Qt_5_9_1_MSVC2015_64bit-Debug\
+rd /s /q .\build-locAlgrithom-Desktop_Qt_5_9_1_MSVC2015_64bit-Debug
+echo delete .\build-locAlgrithom-Desktop_Qt_5_9_1_MSVC2015_64bit-Release\
+rd /s /q .\build-locAlgrithom-Desktop_Qt_5_9_1_MSVC2015_64bit-Release
+echo clean finished
+pause
